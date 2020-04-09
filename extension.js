4bcc17f94b3790a305ad8e8e185e95e080eb7a13
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2020-04-10 00:29:15 +0530

    Add of quote

--- extension.js
+++ extension.js
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const http = require('https');
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -8,28 +9,46 @@ const vscode = require('vscode');
 /**
  * @param {vscode.ExtensionContext} context
  */
+
+let interval = null;
+
+function paintQuote(status) {
+	try {
+		http.get("https://thesimpsonsquoteapi.glitch.me/quotes", (response) => {
+			response.on('data', function (chunk) {
+				const t = JSON.parse('' + chunk);
+				const { quote, character } = t[0];
+				status.text = `${quote} - ${character}`;
+				status.show();
+			});
+		});
+	} catch (error) {
+		status.text = "Your luck is so bad! We couldn't get a quote";
+		status.show();
+	}
+}
+
+function resetDebounce(status){
+	clearTimeout(interval);
+	interval = setTimeout(() => {
+		paintQuote(status);
+	}, 2000);
+}
+
 function activate(context) {
-
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "simpsons-quotes" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('simpsons-quotes.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Simpsons Quotes!');
+	const status = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 100);
+	context.subscriptions.push(status);
+	vscode.workspace.onDidChangeTextDocument((e)=>{
+		status.hide();
+		resetDebounce(status);
 	});
-
-	context.subscriptions.push(disposable);
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() {
+	clearTimeout(interval);
+}
 
 module.exports = {
 	activate,
